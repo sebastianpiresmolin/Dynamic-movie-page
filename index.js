a9fd14a9df884a94ce68bbf14863cf95f4a9f1ed
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-01-19 19:15:49 +0100

    mock SSR site with partials

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import fs from "fs/promises";
-import handlebars from "handlebars";
+import express from 'express';
+import { engine } from 'express-handlebars';
+import fs from 'fs/promises';
+import handlebars from 'handlebars';
+import path from 'path';
 
 const app = express();
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./templates");
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
 
 const MENU = [
-  { name: "Home", link: "/" },
-  { name: "About", link: "/about" },
-  { name: "Contact", link: "/contact" },
+  { name: 'Home', link: '/' },
+  { name: 'About', link: '/about' },
+  { name: 'Contact', link: '/contact' },
+];
+
+const images = [
+  {
+    index:
+      'https://static01.nyt.com/images/2023/12/12/climate/12cli-cats/12cli-cats-jumbo.jpg?quality=75&auto=webp',
+  },
+  {
+    about: 'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg',
+  },
+  {
+    contact:
+      'https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg?w=900',
+  },
 ];
 
 async function renderPage(response, page) {
+  const activePage = page === '/' ? index : page;
+  const currentPath = page == 'index' ? '/' : `/${page}`
+  const activeImage =
+    page === '/' ? 0 : page === 'about' ? 1 : page === 'contact' ? 2 : 0;
+  const activeImageIndex = images[activeImage][activePage];
   response.render(page, {
-    menuItems: MENU
+    menuItems: MENU.map(item => {
+      return {
+        active: currentPath == item.link,
+        name: item.name,
+        link: item.link,
+      };
+    }),
+    image: activeImageIndex,
   });
 }
 
-app.get("/", async (request, response) => {
-  renderPage(response, "index");
+app.get('/', async (request, response) => {
+  renderPage(response, 'index');
 });
 
-app.get("/about", async (request, response) => {
-  renderPage(response, "about");
+app.get('/about', async (request, response) => {
+  renderPage(response, 'about');
 });
 
-app.get("/contact", async (request, response) => {
-  renderPage(response, "contact");
+app.get('/contact', async (request, response) => {
+  renderPage(response, 'contact');
 });
 
 app.use(express.static('static'));
