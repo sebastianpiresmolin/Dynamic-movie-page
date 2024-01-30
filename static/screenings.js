555b49298a360f9270181752e98dcc590f648f63
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-01-30 16:21:02 +0100

    screenings are now displaying on index

--- static/screenings.js
+++ static/screenings.js
@@ -1,9 +1,7 @@
-let screenings = [];
+let screenings;
 
 async function screening() {
-  const response = await fetch(
-    '/app/home/screenings'
-  );
+  const response = await fetch('/app/home/screenings');
   screenings = await response.json();
   return screenings;
 }
@@ -11,5 +9,26 @@ async function screening() {
 window.onload = async function () {
   const result = await screening();
   console.log(result);
-  console.log(screenings);
+  for (let i = 0; i < result.screenings.length; i++) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.href = `/screenings/${result.screenings[i].id}`;
+    a.textContent = result.screenings[i].title;
+
+    let img = document.createElement('img');
+    img.src = result.screenings[i].image;
+    a.appendChild(img);
+
+    let room = document.createElement('p');
+    room.textContent = result.screenings[i].room;
+
+    let date = document.createElement('p');
+    date.textContent = result.screenings[i].time;
+
+    li.appendChild(a);
+    li.appendChild(room);
+    li.appendChild(date);
+    console.log(result.screenings[i]);
+    document.getElementById('screenings').appendChild(li);
+  }
 };
