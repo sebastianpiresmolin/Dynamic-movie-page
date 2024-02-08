9bbd5aa928c1be8b6107766d0caa2a36a950f71e
William3321 <122247630+William3321@users.noreply.github.com>
2024-02-08 09:36:19 +0100

    Update rating.spec.js

--- tests/rating.spec.js
+++ tests/rating.spec.js
@@ -1,1 +1,38 @@
+import fetch from 'node-fetch';
 
+async function fetchMovieTitlesFromYourAPI() {
+    try {
+        const response = await fetch('https://plankton-app-xhkom.ondigitalocean.app/api/movies');
+        const data = await response.json();
+        
+        const movieTitles = data.data.map(movie => movie.attributes.title);
+
+        return movieTitles;
+    } catch (error) {
+        console.error('Error fetching movie titles from your API:', error);
+        return [];
+    }
+}
+
+async function fetchMovieDataFromOMDB(movieTitle) {
+    const response = await fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=eb137354`);
+    const data = await response.json();
+    return data;
+}
+
+describe('OMDB API Test Suite', () => {
+    test('Fetch and test movie ratings from your API', async () => {
+        const movieTitles = await fetchMovieTitlesFromYourAPI();
+        for (const movieTitle of movieTitles) {
+            const movieData = await fetchMovieDataFromOMDB(movieTitle);
+
+            console.log(`Movie Data for "${movieTitle}":`, movieData.imdbRating);
+            expect(movieData.Ratings.length).toBeGreaterThan(0);
+            expect(movieData.Ratings[0].Source).toBeDefined();
+        
+            if (typeof movieData.imdbRating !== 'undefined') {
+                expect(movieData.Ratings[0].Value).toBeDefined();
+            }
+        }
+    });
+});
