73c5e87f67cc9d8c0c6c83a60334cf723fa88837
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-02-25 16:10:57 +0100

    finished signup logic and login logic

--- static/login.js
+++ static/login.js
@@ -1,5 +1,6 @@
 const signupPassword = document.getElementById('signupPassword');
 
+//Password Strength logic
 if (signupPassword) {
   signupPassword.addEventListener('keyup', () => {
     const emptyCircleOne = document.getElementById('emptyCircleOne');
@@ -40,3 +41,72 @@ if (signupButton) {
     window.location.href = './signup';
   });
 }
+
+//New user registration
+const form = document.getElementById('signupForm');
+const signupPageButton = document.getElementById('signupPageButton');
+let users = JSON.parse(localStorage.getItem('users')) || [];
+window.users = users;
+
+if (form && signupPageButton) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let newUser = {};
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const signupPassword = document.getElementById('signupPassword').value;
+
+    newUser.name = firstName;
+    newUser.lastName = lastName;
+    newUser.password = signupPassword;
+    newUser.email = email;
+    newUser.phone = phone;
+    users.push(newUser);
+
+    localStorage.setItem('users', JSON.stringify(users)); // Store users in localStorage
+
+    window.location.href = './login';
+  });
+}
+
+const loginForm = document.getElementById('loginForm');
+const loginButton = document.getElementById('loginButton');
+
+if (loginForm && loginButton) {
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('usernameLogin').value;
+    const password = document.getElementById('passwordLogin').value;
+
+    const users = JSON.parse(localStorage.getItem('users')) || []; // Retrieve users from localStorage
+
+    const usernameTry = users.find((user) => user.email === username);
+
+    if (usernameTry && usernameTry.password === password) {
+      // Check if usernameTry is not undefined
+      document.getElementById('logoutButton').style.display = 'block';
+      let loginLink = Array.from(document.querySelectorAll('.menuItem a')).find(a => a.textContent === 'Login');
+      
+      window.location.href = '/';
+      console.log('Logged in');
+    } else {
+      document.getElementById('loginError').style.display = 'block';
+      console.log('Wrong password or user not found');
+    }
+  });
+}
+
+/*const users = [
+  {name: 'sebastian',
+  password: 123},
+  {name: 'erik',
+  password: 123}
+];
+
+const erik = users.find(user => user.name === 'erik');
+console.log(erik.password); 456
+*/
